c9f48fd0fb3ac2dfa452534f2379358727b2d0ae
ashish-greycube <mr.ashish.shah@gmail.com>
2023-07-18 15:15:10 +0530

    fix: create field

--- sw/patches/poi_custom_field.py
+++ sw/patches/poi_custom_field.py
@@ -2,6 +2,7 @@ import frappe
 from frappe.custom.doctype.custom_field.custom_field import create_custom_fields
 
 def execute():
+    print("creating sales_order_item_cf in POI")
     custom_fields = {
         "Purchase Order Item": [
             dict(
@@ -9,6 +10,7 @@ def execute():
                 label="Sales Order Item CF",
                 fieldtype="Data",
                 insert_after="sales_order_item",
+                translatable=0
             ),    
         ]   
     }
